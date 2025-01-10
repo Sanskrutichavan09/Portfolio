b13b051b16add0ba85293f3b365e7c657083b1aa
sanskruti chavam <sanskrutichavan45@gmail.com>
2025-01-10 21:11:33 +0530

    Add files via upload

---
+++ src/componets/MainContainer/MainContainer.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { motion } from "framer-motion";
+
+function MainContainer({ children }) {
+  return (
+    <motion.div
+      initial={{ y: -20, opacity: 0, scale: 0.9 }}
+      animate={{ y: 0, opacity: 1, scale: 1 }}
+      transition={{ duration: ".3", stiffness: "150" }}
+      exit={{opacity:0, scale:.9}}
+      className="component_container"
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default MainContainer;
