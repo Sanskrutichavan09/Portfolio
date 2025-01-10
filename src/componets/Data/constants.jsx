bd40114934b24453caf3bd2ad022061e6dabb29d
sanskruti chavam <sanskrutichavan45@gmail.com>
2025-01-10 21:03:58 +0530

    Add files via upload

---
+++ src/componets/Data/constants.jsx
@@ -0,0 +1,45 @@
+export const Bio = {
+    name: "Sanskruti Chavan",
+    roles: ["Full Stack Developer","React-Developer", "Programmer"],
+    description:
+      " I am a creative and detail-oriented frontend developer with a passion for crafting seamless and engaging user interfaces. With a strong focus on delivering intuitive and responsive web experiences, I am proficient in HTML, CSS, JavaScript, and modern frameworks like React. I enjoy transforming complex ideas into user-friendly designs and am committed to continuous learning to stay updated with the latest web technologies.",
+    github: "https://github.com/Sanskrutichavan09",
+    gmail: "Sanskrutichavan45@gmail.com",
+    resume: "https://drive.google.com/file/d/1gtfKgX_rRJVBPnwSQ7T4TjUM3R7IW_jU/view?usp=sharing", // Add your resume link
+    linkedin: "https://www.linkedin.com/in/sanskruti-chavan-web-developer/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend Development",
+      icon: "React.webp", // Unique icon for this skill
+      skills: [
+        { name: "HTML" },
+        { name: "CSS" },
+        { name: "JavaScript" },
+        { name: "React.js" },
+      ],
+    },
+    {
+      title: "Backend Development",
+      icon: "backend.png", // Unique icon for this skill
+      skills: [
+        { name: "Node.js" },
+        { name: "Express.js" },
+        { name: "MongoDB" },
+      
+      ],
+    },
+    {
+      title: "Other",
+      icon: "otherr.png", // Unique icon for this skill
+      skills: [
+        {name:"java"},
+      
+        { name: "Networking-(Basic)" },
+        { name: "Cloud-(Basic)" },
+        {name:"wordpress"}
+      ],
+    },
+  ];
+  
